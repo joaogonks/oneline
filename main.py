6ad4688c86f13330f289dff7b8e961b5115afd6b
João Costa <joaocosta@joaos-imac.media.mit.edu>
2018-01-22 18:31:57 -0500

    adding second stepper

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 import Easydriver as ed
+import threading
+import time
 
 MM_PER_STEP = 0.036
 MM_COUNTER = 0
@@ -30,30 +32,105 @@ stepper_pen = ed.easydriver(27, 0.001, 22, 26, 19)
 
 # Set motor direction to clockwise.
 
+class Z_Motor(threading.Thread):
+	def __init__(self, bus, deviceId):
+        threading.Thread.__init__(self)
+        self.motor = ed.easydriver(27, 0.001, 22, 26, 19)
+        self.motor.set_eighth_step()
+        self.mm_per_step_z = 0.003636
 
-stepper_spool.set_direction(cw)
-stepper_spool.set_eighth_step()
+    def initial_sequence(self):
+    	self.move_down(6)
+    	time.sleep(1)
+    	self.move_up(6)
+    	time.sleep(1)
 
-stepper_pen.set_direction(cw)
-stepper_pen.set_eighth_step()
+   	def move_down(self,mm):
+   		print "moving down: %s mm" % (mm)
+   		self.steps_to_move = mm/self.mm_per_step_z
+   		self.motor.set_direction(cw)
+   		for i in range(0,self.steps_to_move):
+   			self.motor.step()
+   	def move_up(self,mm):
+   		print "moving up: %s mm" % (mm)
+   		self.steps_to_move = mm/self.mm_per_step_z
+   		self.motor.set_direction(ccw)
+   		for i in range(0,self.steps_to_move):
+   			self.motor.step()
 
-# for i in range (0,999):
-# 	stepper_spool.step()
+   	def run(self):
+   		self.initial_sequence()
+
+   		while True:
+   			print "waiting for information"
+   			print "waiting for information."
+   			print "waiting for information.."
+   			print "waiting for information..."
+
+class Spool(threading.Thread):
+	def __init__(self, bus, deviceId):
+        threading.Thread.__init__(self)
+        self.motor = ed.easydriver(18, 0.004, 23, 24, 17)
+        self.motor.set_eighth_step()
+        self.mm_per_step_spool = 0.036
+
+    def initial_sequence(self):
+    	self.move_down(5)
+    	time.sleep(1)
+
+   	def move_forward(self,mm):
+   		print "moving down: %s mm" % (mm)
+   		self.steps_to_move = mm/self.mm_per_step_spool
+   		self.motor.set_direction(cw)
+   		for i in range(0,self.steps_to_move):
+   			self.motor.step()
+   	def move_back(self,mm):
+   		print "moving up: %s mm" % (mm)
+   		self.steps_to_move = mm/self.mm_per_step_spool
+   		self.motor.set_direction(ccw)
+   		for i in range(0,self.steps_to_move):
+   			self.motor.step()
+
+   	def run(self):
+   		self.initial_sequence()
+   		while True:
+   			print "waiting for information"
+   			print "waiting for information."
+   			print "waiting for information.."
+   			print "waiting for information..."
+
+spool = Spool()
+spool.start()
+
+z_motor = Z_Motor()
+z_motor.start()    			
+
+
+
+
+
+# stepper_spool.set_direction(cw)
+# stepper_spool.set_eighth_step()
+
+# stepper_pen.set_direction(cw)
+# stepper_pen.set_eighth_step()
+
+# # for i in range (0,999):
+# # 	stepper_spool.step()
+# # 	MM_COUNTER = MM_COUNTER + MM_PER_STEP
+# # 	print MM_COUNTER
+
+# for i in range (0,1650):
+# 	stepper_pen.step()
 # 	MM_COUNTER = MM_COUNTER + MM_PER_STEP
 # 	print MM_COUNTER
 
-for i in range (0,1650):
-	stepper_pen.step()
-	MM_COUNTER = MM_COUNTER + MM_PER_STEP
-	print MM_COUNTER
+# stepper_pen.set_direction(ccw)
 
-stepper_pen.set_direction(ccw)
-
-for i in range (0,1650):
-	stepper_pen.step()
-	MM_COUNTER = MM_COUNTER + MM_PER_STEP
-	print MM_COUNTER
+# for i in range (0,1650):
+# 	stepper_pen.step()
+# 	MM_COUNTER = MM_COUNTER + MM_PER_STEP
+# 	print MM_COUNTER
 
 
-# Clean up (just calls GPIO.cleanup() function.)
-stepper_spool.finish()
+# # Clean up (just calls GPIO.cleanup() function.)
