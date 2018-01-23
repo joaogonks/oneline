65936c32cbf82fd21f5f38a38d00fadcebc33a45
João Costa <joaocosta@joaos-imac.media.mit.edu>
2018-01-22 19:20:50 -0500

    adding second stepper

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import Easydriver as ed
 import threading
 import time
+import Queue
 
 MM_PER_STEP = 0.036
 MM_COUNTER = 0
@@ -10,6 +11,7 @@ MM_COUNTER = 0
 cw = True
 ccw = False
 
+test_list = [(5,True),(10,False), (20,True),(45,False)]
 """
 Arguments to pass or set up after creating the instance.
 Step GPIO pin number.
@@ -46,28 +48,33 @@ class Z_Motor(threading.Thread):
         time.sleep(1)
 
     def move_down(self,mm):
-        print "moving down: %s mm" % (mm)
+        #print "moving down: %s mm" % (mm)
         self.steps_to_move = int(mm/self.mm_per_step_z)
         self.motor.set_direction(cw)
         for i in range(0,self.steps_to_move):
             self.motor.step()
     def move_up(self,mm):
-        print "moving up: %s mm" % (mm)
+        #print "moving up: %s mm" % (mm)
         self.steps_to_move = int(mm/self.mm_per_step_z)
         self.motor.set_direction(ccw)
         for i in range(0,self.steps_to_move):
             self.motor.step()
+    def receive(self, value):
+        if value:
+            self.move_down(6)
+        else:
+            self.move_up(6)
 
     def run(self):
         print "starting pen..."
         self.initial_sequence()
-        while True:
-            self.initial_sequence()
-            time.sleep(5)
-            # print "[Pen] waiting for information"
-            # print "[Pen] waiting for information."
-            # print "[Pen] waiting for information.."
-            # print "[Pen] waiting for information..."
+        # while True:
+        #     self.initial_sequence()
+        #     time.sleep(5)
+        #     # print "[Pen] waiting for information"
+        #     # print "[Pen] waiting for information."
+        #     # print "[Pen] waiting for information.."
+        #     # print "[Pen] waiting for information..."
 
 class Spool(threading.Thread):
     def __init__(self):
@@ -78,16 +85,18 @@ class Spool(threading.Thread):
 
     def initial_sequence(self):
         self.move_forward(5)
+        MM_COUNTER = 0
         time.sleep(1)
 
-    def move_forward(self,mm):
-        print "moving down: %s mm" % (mm)
+    def move_forward(self,mm, mm2):
+        print "moving forward: %s mm" % (mm)
         self.steps_to_move = int(mm/self.mm_per_step_spool)
         self.motor.set_direction(cw)
         for i in range(0,self.steps_to_move):
             self.motor.step()
+        MM_COUNTER = MM_COUNTER + mm2
     def move_back(self,mm):
-        print "moving up: %s mm" % (mm)
+        #print "moving up: %s mm" % (mm)
         self.steps_to_move = int(mm/self.mm_per_step_spool)
         self.motor.set_direction(ccw)
         for i in range(0,self.steps_to_move):
@@ -103,11 +112,35 @@ class Spool(threading.Thread):
         while True:
             self.move_forward(1)
 
-spool = Spool()
-spool.start()
+class Main(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.queue = Queue.Queue()
+        self.spool = Spool()
+        self.z_motor = Z_Motor()
 
-z_motor = Z_Motor()
-z_motor.start()    			
+
+    def update_value(self, info):
+        self.queue.put(info)
+
+    def run(self):
+        for item in test_dict:
+            self.steps_to_take = (item[0][0]/MM_PER_STEP) - MM_COUNTER
+            self.spool.move_forward(item[0], self.steps_to_take)
+            self.z_motor.receive(item[1])
+
+main = Main()
+main.start()
+
+
+
+
+
+# spool = Spool()
+# spool.start()
+
+# z_motor = Z_Motor()
+# z_motor.start()    			
 
 
 
