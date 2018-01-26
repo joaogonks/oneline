1b3a675b2faf614cc551f3d5991f5ded87c6e3b4
João Costa <joaocosta@joaos-imac.media.mit.edu>
2018-01-26 18:00:36 -0500

    weird

--- main-encoder.py
+++ main-encoder.py
@@ -181,9 +181,9 @@ class Main(threading.Thread):
             #print "before while"
             self.encoder.set_zero()
             while not item[0] - self.tolerance <= self.current_position <= item[0] + self.tolerance:
-                self.current_position = self.queue.get(True,None)
                 print "moving forward"
                 self.spool.move_step_forward()
+                self.current_position = self.queue.get(True,None)
                 print self.current_position
             self.z_motor.receive(item[1])
             print "We're at: %smm" % (self.current_position)
