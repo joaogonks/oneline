ade160abf392e743bab5f236cc40e2c6e76a023b
João Costa <joaocosta@joaos-imac.media.mit.edu>
2018-01-29 15:10:16 -0500

    adding dc motor support

--- main-encoder-dc.py
+++ main-encoder-dc.py
@@ -203,18 +203,19 @@ class Main(threading.Thread):
 
     def run(self):
         self.encoder.start()
-        self.initial_sequence()
+        #self.initial_sequence()
         for item in test.test_list:
             print "HERE"
             self.current_position = self.queue.get(True,None)
             while not item[0] - self.tolerance <= self.current_position <= item[0] + self.tolerance:
                 self.current_position = self.queue.get(True,None)
+                print self.current_position
                 self.dc_motor.start_motor()
                 print self.current_position
             self.dc_motor.stop_motor()
             self.z_motor.receive(item[1])
             print "We're at: %smm" % (self.current_position)
-        self.initial_sequence()
+        #self.initial_sequence()
 
 main = Main()
 main.start()
