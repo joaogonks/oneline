e24d160f4f73a9f806f60a2d727b6f93aa2804aa
João Costa <joaocosta@joaos-imac.media.mit.edu>
2018-01-29 20:56:15 -0500

    adding dc motor support

--- main-encoder-dc.py
+++ main-encoder-dc.py
@@ -135,7 +135,7 @@ class Encoder(threading.Thread):
         elif self.last_position - current_position < 0 and not self.direction:
             self.lap -= 1
         self.last_position = current_position
-        #print "HELLO", abs(4096 - ((self.lap*self.resolution) + current_position))-4096
+        print "HELLO", abs(4096 - ((self.lap*self.resolution) + current_position))-4096
         return abs(4096 - ((self.lap*self.resolution) + current_position))-4096
 
     def set_zero(self):
