535fe165cc340ceae01d49e09da4d7df2a0535c1
João Costa <joaocosta@joaos-imac.media.mit.edu>
2018-02-13 17:51:07 -0500

    changing tolerance

--- main-encoder-dc.py
+++ main-encoder-dc.py
@@ -182,7 +182,7 @@ class Main(threading.Thread):
         self.encoder = Encoder(0,1)
         self.current_position = None
         self.last_position = None
-        self.tolerance = 0.1
+        self.tolerance = 0.2
 
     def initial_sequence(self):
         print "Running initial sequence..."
