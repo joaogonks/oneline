d79111e278cd51074ad209c40de0c1ae5ed44dea
João Costa <joaocosta@joaos-imac.media.mit.edu>
2018-09-03 13:44:07 -0400

    adding bytearrayconverter

--- AMT203pigpio.py
+++ AMT203pigpio.py
@@ -31,6 +31,7 @@ class AMT203():
         msb_result = self.pi.spi_xfer(self.spi,[0x00])
         lsb_result = self.pi.spi_xfer(self.spi,[0x00])
         print "MSB: %s | LSB: %s " % (msb_result, lsb_result)
+        print msb_result[1]
         msb = self.bytes_to_int(msb_result[1][0])
         lsb = self.bytes_to_int(lsb_result[1][0])
         # msb_bin = bin(msb_result[0]<<8)[2:]
