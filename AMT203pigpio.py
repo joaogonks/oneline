f253b4ea8a131d3621e9dad8747613a47091a1f7
João Costa <joaocosta@joaos-imac.media.mit.edu>
2018-09-03 13:23:56 -0400

    adding bytearrayconverter

--- AMT203pigpio.py
+++ AMT203pigpio.py
@@ -41,7 +41,7 @@ class AMT203():
         first_result = self.pi.spi_xfer(self.spi,[0x70])
         while first_result[1] != b'\80':
           print first_result[1]
-          first_result = self.pi.spi_xfer(self.spi,[0x10])
+          first_result = self.pi.spi_xfer(self.spi,[0x00])
         print "Zero set was successful and the new position offset is stored in EEPROM"
         self.clean_buffer()
         # GPIO.output(self.pin, GPIO.LOW)
